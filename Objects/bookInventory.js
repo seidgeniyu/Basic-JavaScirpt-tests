9bb2d79222c386d9c36b8d585d2460b1ebe848a7
Seid Geniyu <seyageniyu@gmail.com>
2024-03-06 13:05:33 +0300

    book inventory

---
+++ Objects/bookInventory.js
@@ -0,0 +1,17 @@
+const bookInventory = {
+  // First book
+  "ከአርሽ በላይ": {
+    title: "ከአርሽ በላይ",
+    author: "Ibnu munewor",
+    pages: 250,
+    available: true,
+  },
+
+  // Second book
+  "ኢብኑ ተይሚያ የሱናው አንበሳ": {
+    title: "ኢብኑ ተይሚያ የሱናው አንበሳ",
+    author: "Ibnu munewor",
+    pages: 324,
+    available: true,
+  },
+};
